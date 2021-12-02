27b3368d72b37328ee6bb5981a3901057e428c12
Mohamed <mohamedelsanour1996@gmail.com>
2021-12-02 21:14:48 +0200

    Add project files.

---
+++ Migrations/20211202181911_Init2.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication4.Migrations
+{
+    public partial class Init2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmployeetName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Salary = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication4.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId
+        {
+            get;
+            set;
+        }
+       
+        public string EmployeetName
+        {
+            get;
+            set;
+        }
+        public double Salary
+        {
+            get;
+            set;
+        }
+       
+    }
+}
